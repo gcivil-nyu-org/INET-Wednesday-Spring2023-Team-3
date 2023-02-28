cb50af489ac6e96d4cec44c34d56986f5fad60ea
SolidifiedRay <yc3346@nyu.edu>
2023-02-28 13:06:54 -0800

    Fix eslint error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -91,14 +91,6 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
-
 function App() {
   const [data, setData] = useState([]);
 
