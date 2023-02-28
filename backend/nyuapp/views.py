f2180bb1bec6ec41887b0be7eb82cc5608ba3772
SolidifiedRay <yc3346@nyu.edu>
2023-02-28 07:29:28 -0800

    Create initial django mvp

---
+++ backend/nyuapp/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+
+from .models import Todo
+from .serializers import TodoSerializer
+
+class ListTodo(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
