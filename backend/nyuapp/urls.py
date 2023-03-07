815955fba098d92176433376b8b467ee0f304c8e
ramanarayanan-98 <ramanarayanan.s.1998@gmail.com>
2023-03-07 14:27:54 -0500

    Add filtering and searching for questions

--- backend/nyuapp/urls.py
+++ backend/nyuapp/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('questions/',views.get_questions, name = "get_questions"),
-    path('questions/cur_page/<int:cur_page>/single_page_count/<int:single_page_count>', views.get_paginated_questions, name = "get_paginated_questions")
 ]
--- backend/nyuapp/utils/add_bulk_questions.py
+++ backend/nyuapp/utils/add_bulk_questions.py
@@ -1,3 +1,4 @@
+# Script 1
 od = ["Easy","Medium","Hard","Expert"]
 import csv
 from nyuapp.models import Question
@@ -11,3 +12,26 @@ with open("<path_to_csv>") as f:
             difficulty=od[random.randint(0,3)],
             type="Behavioural",
             )
+
+# Script 2
+from nyuapp.models import Question
+import random     
+companies = ["Google", "Amazon", "Meta", "Bloomberg", "Tesla", "Microsoft", "Apple", "Netflix"]
+data = Question.objects.all()
+for row in data:
+    cur_num = random.randint(1,8)
+    current_picks = random.sample(companies,cur_num)
+    row.companies = ",".join(current_picks)
+    row.save()
+
+# Script 3
+from nyuapp.models import Company
+companies = ["Google", "Amazon", "Meta", "Bloomberg", "Tesla", "Microsoft", "Apple", "Netflix"]
+for c in companies:
+    Company.objects.get_or_create(name=c)
+
+# Script 4
+from nyuapp.models import Difficulty
+diff = ["Easy","Medium","Hard","Expert"]
+for d in diff:
+    Difficulty.objects.get_or_create(text=d)
