9cba2e3f3cb7b352e59d4fbc18babe62f69d4fe1
Andrew Tavcar <drewtavcar@Andrews-MacBook-Pro.local>
2023-03-06 23:23:47 -0500

    User Registration/Login

--- backend/nyuapp/serializers.py
+++ backend/nyuapp/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Todo
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.models import User
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,3 +11,25 @@ class TodoSerializer(serializers.ModelSerializer):
             'description',
         )
         model = Todo
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True, validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        max_length=32, validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(min_length=8, write_only=True)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data["first_name"],
+            validated_data["last_name"],
+            validated_data["email"],
+            validated_data["password"],
+        )
+        return user
+
+    class Meta:
+        model = User
+        fields = ("id", "first_name", "last_name", "email", "password")
--- backend/nyuapp/urls.py
+++ backend/nyuapp/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('questions/',views.get_questions, name = "get_questions"),
-    path('questions/cur_page/<int:cur_page>/single_page_count/<int:single_page_count>', views.get_paginated_questions, name = "get_paginated_questions")
+    path("questions/", views.get_questions, name="get_questions"),
+    path(
+        "questions/cur_page/<int:cur_page>/single_page_count/<int:single_page_count>",
+        views.get_paginated_questions,
+        name="get_paginated_questions",
+    ),
+    path("register/", views.UserCreate.as_view(), name="account-create"),
 ]
