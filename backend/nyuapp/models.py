b489c22464e2253f92e29469019bb11a6f00ac25
ramanarayanan-98 <ramanarayanan.s.1998@gmail.com>
2023-03-17 21:48:18 -0400

    Todo Model removed

--- backend/nyuapp/models.py
+++ backend/nyuapp/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 
 
-class Todo(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-
-    def __str__(self):
-        """A string representation of the model."""
-        return self.title
 
 class Question(models.Model):
     q_id = models.BigAutoField(primary_key=True)
