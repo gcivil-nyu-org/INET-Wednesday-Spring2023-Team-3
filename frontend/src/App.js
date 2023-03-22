b8aaeaa59ff1bbcdd15f4f1c222316ad814155ad
makendym <mmidouin18@stac.edu>
2023-03-22 14:42:31 -0400

    commented unused variables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import * as React from "react";
 import SignInSide from "./Components/login";
 import Homepage from "./Components/home";
-import SignUp from "./Components/signup";
+// import SignUp from "./Components/signup";
 import UploadQuestion from "./Components/uploadQuestion";
 import QuestionDetails from "./Components/questionDetails";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import PrivateRoute from "./utils/PrivateRoute";
-import ProtectedPage from "./Views/utils_ProtectedPage";
+// import PrivateRoute from "./utils/PrivateRoute";
+// import ProtectedPage from "./Views/utils_ProtectedPage";
 import Login from "./Views/loginPage";
 import Register from "./Views/registerPage"
 function App() {
