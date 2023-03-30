b307b514da3550fafe747de5460f24fd1657f564
makendym <mmidouin18@stac.edu>
2023-03-30 18:59:45 -0400

    Revert "Updated register page"
    This reverts commit 4379c123f0fa7704d2617362ec6afe2b250d236b.

--- frontend/src/Views/registerPage.js
+++ frontend/src/Views/registerPage.js
@@ -19,9 +19,6 @@ import Select from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import { FormControl } from "@mui/material";
-import Stack from "@mui/material/Stack";
-import Snackbar from "@mui/material/Snackbar";
-import MuiAlert from "@mui/material/Alert";
 
 function Copyright(props) {
   return (
@@ -40,26 +37,11 @@ function Copyright(props) {
     </Typography>
   );
 }
-const Alert = React.forwardRef(function Alert(props, ref) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-});
-
 function Register() {
-  const [open, setOpen] = React.useState(false);
-
-
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-
-    setOpen(false);
-  };
-
   const { registerUser } = useContext(AuthContext);
 
   const [formData, setFormData] = useState({
-    userType: "",
+    userType: "Student/Alumni",
     first_name: "",
     last_name: "",
     email: "",
@@ -67,13 +49,7 @@ function Register() {
   });
 
   const handleInputChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    if (name === "userType") {
-      setFormData({ ...formData, userType: value });
-    } else {
-      setFormData({ ...formData, [name]: value });
-    }
+    setFormData({ ...formData, [event.target.name]: event.target.value });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -84,9 +60,8 @@ function Register() {
       formData.last_name,
       formData.password1
     );
-    setOpen(true);
-    console.log(formData);
-    
+    console.log(" Successful register");
+    alert(" Successful register");
   };
   const navigate = useNavigate();
   const loginPage = () => {
@@ -97,133 +72,122 @@ function Register() {
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
-        <Stack spacing={2} sx={{ width: "100%" }}>
-          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-            <Alert
-              onClose={handleClose}
-              severity="success"
-              sx={{ width: "100%" }}
-            >
-               Registration Successful! Please verify Email Address!
-            </Alert>
-          </Snackbar>
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
           <Box
-            sx={{
-              marginTop: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
           >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign up
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 3 }}
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <FormControl fullWidth required>
+                  <InputLabel id="demo-simple-select-label">
+                    Type of User
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    label="Type of User"
+                    value={formData.userType}
+                    onChange={handleInputChange}
+                    autoWidth
+                  >
+                    <MenuItem value={10}>Student/Alumni</MenuItem>
+                    <MenuItem value={20}>Hiring Manager</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="first_name"
+                  required
+                  fullWidth
+                  id="first_name"
+                  label="First Name"
+                  value={formData.first_name}
+                  onChange={handleInputChange}
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="last_name"
+                  label="Last Name"
+                  name="last_name"
+                  value={formData.last_name}
+                  onChange={handleInputChange}
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  placeholder="@nyu.edu"
+                  name="email"
+                  autoComplete="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password1"
+                  label="Password"
+                  type="password"
+                  id="password1"
+                  value={formData.password1}
+                  onChange={handleInputChange}
+                  autoComplete="new-password"
+                />{" "}
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
             >
-              <Grid container spacing={3}>
-                <Grid item xs={12}>
-                  <FormControl fullWidth required>
-                    <InputLabel id="demo-simple-select-label">
-                      Type of User
-                    </InputLabel>
-                    <Select
-                      labelId="userType"
-                      name="userType"
-                      id="userType"
-                      label="Type of User"
-                      value={formData.userType}
-                      onChange={handleInputChange}
-                      autoWidth
-                      autoFocus
-                    >
-                      <MenuItem value="Student/Alumni">Student/Alumni</MenuItem>
-                      <MenuItem value="Hiring Manager">Hiring Manager</MenuItem>
-                    </Select>
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    autoComplete="given-name"
-                    name="first_name"
-                    required
-                    fullWidth
-                    id="first_name"
-                    label="First Name"
-                    value={formData.first_name}
-                    onChange={handleInputChange}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="last_name"
-                    label="Last Name"
-                    name="last_name"
-                    value={formData.last_name}
-                    onChange={handleInputChange}
-                    autoComplete="family-name"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    value={formData.email}
-                    onChange={handleInputChange}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password1"
-                    label="Password"
-                    type="password"
-                    id="password1"
-                    value={formData.password1}
-                    onChange={handleInputChange}
-                    autoComplete="new-password"
-                  />{" "}
-                </Grid>
-                <Grid item xs={12}>
-                  <FormControlLabel
-                    control={
-                      <Checkbox value="allowExtraEmails" color="primary" />
-                    }
-                    label="I want to receive inspiration, marketing promotions and updates via email."
-                  />
-                </Grid>
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link component="button" onClick={loginPage} variant="body2">
+                  Already have an account? Sign in
+                </Link>
               </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign Up
-              </Button>
-              <Grid container justifyContent="flex-end">
-                <Grid item>
-                  <Link component="button" onClick={loginPage} variant="body2">
-                    Already have an account? Sign in
-                  </Link>
-                </Grid>
-              </Grid>
-            </Box>
+            </Grid>
           </Box>
-        </Stack>
+        </Box>
         <Copyright sx={{ mt: 5 }} />
       </Container>
     </ThemeProvider>
