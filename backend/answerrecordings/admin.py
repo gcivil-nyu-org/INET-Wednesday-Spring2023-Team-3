3acece7d7dbea5285ca67c38e6a8354719636fe7
ramanarayanan-98 <ramanarayanan.s.1998@gmail.com>
2023-04-03 20:22:07 -0400

    Comment feature added

--- backend/answerrecordings/admin.py
+++ backend/answerrecordings/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import DropBox
+from .models import DropBox, Comment
 
 admin.site.register(DropBox)
+admin.site.register(Comment)
--- backend/answerrecordings/urls.py
+++ backend/answerrecordings/urls.py
@@ -5,12 +5,23 @@ from . import views
 
 router = SimpleRouter()
 router.register("answerrecording", DropBoxViewset)
-urlpatterns = router.urls
 
-urlpatterns += (
+urlpatterns = [
     path(
         "list_answers_for_ques/<int:q_id>",
         views.list_answers_for_ques,
         name="list_answers_for_ques",
     ),
-)
+    path(
+        "get_comments/<int:answer_id>",
+        views.get_comments,
+        name="get_comments",
+    ),
+    path(
+        "post_comment/",
+        views.post_comment,
+        name="post_comment",
+    ),
+]
+
+urlpatterns.extend(router.urls)
