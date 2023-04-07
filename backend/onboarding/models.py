1c4bb3e2626863bc9dc431e74a65d5a4a51cd916
drewcar95 <drewtavcar@icloud.com>
2023-04-07 12:20:32 -0400

    Branch update

--- backend/onboarding/models.py
+++ backend/onboarding/models.py
@@ -6,7 +6,6 @@ from authemail.models import EmailUserManager, EmailAbstractUser
 class MyUser(EmailAbstractUser):
     # Custom fields
     date_of_birth = models.DateField("Date of birth", null=True, blank=True)
-    
 
     # Required
     objects = EmailUserManager()
