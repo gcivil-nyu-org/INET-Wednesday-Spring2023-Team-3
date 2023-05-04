763859cd9cb4926b395e2a4b7e7383a5df4ee143
drewcar95 <drewtavcar@icloud.com>
2023-05-04 00:00:47 -0400

    Deployed Front end.

--- frontend/src/Views/profilePage.js
+++ frontend/src/Views/profilePage.js
@@ -1,19 +1,18 @@
 import { Navigate } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { useContext, useState, useEffect } from "react";
 import Stack from "@mui/material/Stack";
 import Divider from "@mui/material/Divider";
-
+import Card from '@mui/material/Card';
 import AuthContext from "../context/AuthContext";
 import Navbar from "../Components/navbar";
 import { API_ENDPOINT } from "../Components/api";
+
 
 function ProfilePage() {
   const { user } = useContext(AuthContext);
@@ -33,7 +32,8 @@ function ProfilePage() {
       throw error;
     }
   };
-  const [user_type, setUserType] = useState("Not entered yet");
+  const [user_type, setUserType] = useState("Not entered yet")
+  
   // Usage example
   //const email = 'example@example.com'; // Replace with the actual email
   fetchUserType(user.email)
@@ -41,6 +41,7 @@ function ProfilePage() {
     .then(data => {
       console.log(data);
       setUserType(data.user_type || "Not entered yet"); // Access the user_type field from the response data
+      
     })
     .catch(error => {
       // Handle error
@@ -55,7 +56,7 @@ function ProfilePage() {
   const [company, setCompany] = useState("Not entered yet"); //Added
   const [website, setWebsite] = useState("Not entered yet"); //Added
   const [description, setDescription] = useState("Not entered yet"); //Added
-  const [imgFile, setImgFile] = useState("");
+  const [imgFile, setImgFile] = useState("Not entered yet");
 
   useEffect(() => {
     // Fetch data from API and update state variables based on user type
@@ -69,7 +70,7 @@ function ProfilePage() {
           setPreviousEmployer(data.previous_employer || "Not entered yet");
           setLinkedinLink(data.linkedin_link || "Not entered yet");
           setGithubLink(data.github_link || "Not entered yet");
-          setImgFile(data.imgFile || "Not entered yet")
+          setImgFile(data.img_file.url || "Add a Profile Photo")
         })
         .catch((error) => console.error(error));
     } else if (user_type === "Hiring Manager") {
@@ -77,10 +78,10 @@ function ProfilePage() {
           .then((response) => response.json())
           .then((data) => {
             console.log(data);
-            setCompany(data.name || "Not entered yet");
-            setWebsite(data.website || "Not entered yet");
-            setDescription(data.description || "Not entered yet");
-            setImgFile(data.imgFile || "Not entered yet")
+            setCompany(data.name || "Company Name");
+            setWebsite(data.website || "Company Website");
+            setDescription(data.description || "Company Description");
+            setImgFile(data.img_file.url || "Add a Profile Photo")
           })
           .catch((error) => console.error(error));
       }
@@ -97,91 +98,293 @@ function ProfilePage() {
         <ThemeProvider theme={theme}>
           <Container component="main" maxWidth="xs">
             <CssBaseline />
-            <Box sx={{
-                  marginTop: 5,
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",}}>
-                  <img src={imgFile} alt="Profile" style={{ width: "200px", height: "200px", objectFit: "cover" }} />
-                </Box>
-            <Stack spacing={2} sx={{ width: "100%" }}>
-              <Box
-                sx={{
-                  marginTop: 5,
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                }}
-              >
-                <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-                  <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                  Profile Page
-                </Typography>
-                <div style={{ marginTop: 20, marginBottom: 20 }}>
-                  <Divider variant="middle" width="600px" />
-                </div>
+            {user_type === "Student/Alumni" ? (
+              <>
+              <Card sx={{ marginTop: 5, maxWidth: 345, marginLeft: -45 }}>
+                <Box sx={{
+                      marginTop: 5,
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",}}>
+                      <img src={imgFile} alt="Profile" style={{ width: "200px", height: "200px", objectFit: "cover" }} />
+                    </Box>
+                <Stack spacing={2} sx={{ width: "100%" }}>
+                  <Box
+                    sx={{
+                      marginTop: 5,
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                    }}
+                  >
 
-                {user_type === "Student/Alumni" ? (
-                <>
-                  <Typography
-                    component="h3"
-                    variant="h7"
-                    style={{ marginTop: 10, marginBottom: 10 }}
+                    <Typography component="h1" variant="h5">
+                      {user.email}
+                    </Typography>
+                    <div style={{ marginTop: 20, marginBottom: 20 }}>
+                      <Divider variant="middle" width="600px" />
+                    </div>
+
+                      <Typography
+                        component="h3"
+                        variant="h7"
+                        style={{ marginTop: 10, marginBottom: 10 }}
+                      >
+                        Job Preference: {jobPreference}
+                      </Typography>
+                      <Typography
+                        component="h3"
+                        variant="h7"
+                        style={{ marginTop: 10, marginBottom: 10 }}
+                      >
+                        Years of Experience: {yearsOfExperience}
+                      </Typography>
+                      <Typography
+                        component="h3"
+                        variant="h7"
+                        style={{ marginTop: 10, marginBottom: 10 }}
+                      >
+                        Previous Employer: {previousEmployer}
+                      </Typography>
+                      <Typography
+                        component="h3"
+                        variant="h7"
+                        style={{ marginTop: 10, marginBottom: 10 }}
+                      >
+                        <a href={linkedinLink}>Linkedin</a> |{" "}
+                        <a href={githubLink}>Github</a>
+                      </Typography>
+                    </Box>
+                  </Stack>
+                </Card>
+
+                <Card sx={{ marginTop: -65, maxWidth: 600, }}>
+                  
+                <Box
+                    sx={{
+                      marginTop: 1,
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                    }}
                   >
-                    Job Preference: {jobPreference}
-                  </Typography>
-                  <Typography
-                    component="h3"
-                    variant="h7"
-                    style={{ marginTop: 10, marginBottom: 10 }}
+                    <Typography component="h1" variant="h7">
+                        About
+                      </Typography>
+                      <div style={{ marginTop: 0, marginBottom: 0 }}>
+                        <Divider variant="middle" />
+                      </div>
+
+                        <Typography
+                          component="h3"
+                          variant="h7"
+                          style={{ marginTop: 10, marginBottom: 10 }}
+                        > {jobPreference}
+                        </Typography>
+                    </Box>
+                </Card>
+
+                <Card sx={{ marginTop: 4, maxWidth: 800, }}>
+                  
+                <Box
+                    sx={{
+                      marginTop: 1,
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                    }}
                   >
-                    Years of Experience: {yearsOfExperience}
-                  </Typography>
-                  <Typography
-                    component="h3"
-                    variant="h7"
-                    style={{ marginTop: 10, marginBottom: 10 }}
+                    <Typography component="h1" variant="h7">
+                        User Stats
+                      </Typography>
+                      <div style={{ marginTop: 0, marginBottom: 0 }}>
+                        <Divider variant="middle" />
+                      </div>
+
+                        <Typography
+                          component="h3"
+                          variant="h7"
+                          style={{ marginTop: 5, marginBottom: 5, }}
+                        > Questions Posted | stats
+                        </Typography>
+                        <Typography
+                          component="h3"
+                          variant="h7"
+                          style={{ marginTop: 5, marginBottom: 5, }}
+                        > Questions Answered | stats
+                        </Typography>
+                        <Typography
+                          component="h3"
+                          variant="h7"
+                          style={{ marginTop: 5, marginBottom: 5, }}
+                        > Experiences Posted | stats
+                        </Typography>
+                    </Box>
+                </Card>
+                <Card sx={{ marginTop: -33, marginLeft: 52, marginRight: -50, maxWidth: 800, }}>
+                  
+                <Box
+                    sx={{
+                      marginTop: 1,
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                    }}
                   >
-                    Previous Employer: {previousEmployer}
-                  </Typography>
-                  <Typography
-                    component="h3"
-                    variant="h7"
-                    style={{ marginTop: 10, marginBottom: 10 }}
-                  >
-                    <a href={linkedinLink}>Linkedin</a> |{" "}
-                    <a href={githubLink}>Github</a>
-                  </Typography>
-                </>
-              ) : (
-                <>
-                  <Typography
-                    component="h3"
-                    variant="h7"
-                    style={{ marginTop: 10, marginBottom: 10 }}
-                  >
-                    Company: {company}
-                  </Typography>
-                  <Typography
-                    component="h3"
-                    variant="h7"
-                    style={{ marginTop: 10, marginBottom: 10 }}
-                  >
-                    Website: {website}
-                  </Typography>
-                  <Typography
-                    component="h3"
-                    variant="h7"
-                    style={{ marginTop: 10, marginBottom: 10 }}
-                  >
-                    Description: {description}
-                  </Typography>
-                </>
-              )} 
-              </Box>
-            </Stack>
+                    <Typography component="h1" variant="h7">
+                        Conversations
+                      </Typography>
+                      <div style={{ marginTop: 0, marginBottom: 0 }}>
+                        <Divider variant="middle" />
+                      </div>
+
+                        <Typography
+                          component="h3"
+                          variant="h7"
+                          style={{ marginTop: 5, marginBottom: 5, }}
+                        > User 1
+                        </Typography>
+                        <Typography
+                          component="h3"
+                          variant="h7"
+                          style={{ marginTop: 5, marginBottom: 5, }}
+                        > User 2
+                        </Typography>
+                        <Typography
+                          component="h3"
+                          variant="h7"
+                          style={{ marginTop: 5, marginBottom: 5, }}
+                        > User 3
+                        </Typography>
+                    </Box>
+                </Card>
+              </>
+                  ) : (
+                    <>
+                      <Card sx={{ marginTop: 2, maxWidth: 345, marginLeft: -45, marginRight: 30  }}>
+                        <Box sx={{
+                                marginTop: 5,
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",}}>
+                                <img src={imgFile} alt="Profile" style={{ width: "100px", height: "100px", objectFit: "cover" }} />
+                              </Box>
+                              <div style={{ marginTop: 20, marginBottom: 20 }}>
+                              <Divider variant="middle"  />
+                              </div>
+                              <Box
+                            sx={{
+                              marginTop: 0,
+                              display: "flex",
+                              flexDirection: "column",
+                              alignItems: "center",
+                            }}
+                          >    
+                            <Typography
+                              component="h3"
+                              variant="h7"
+                              style={{ marginTop: 5, marginBottom: 10 }}
+                            >
+                              {company}
+                            </Typography>
+                            <Typography
+                              component="h3"
+                              variant="h7"
+                              style={{ marginTop: 5, marginBottom: 10 }}
+                            >
+                              <a href={website}>Company Website</a> 
+                            </Typography>
+                          </Box>
+
+                      </Card>
+                      <Card sx={{ marginTop: 5, maxWidth: 345, marginLeft: -45 }}>
+                        <Box sx={{
+                              marginTop: 5,
+                              display: "flex",
+                              flexDirection: "column",
+                              alignItems: "center",}}>
+                              <img src={imgFile} alt="Profile" style={{ width: "75px", height: "75px", objectFit: "cover" }} />
+                            </Box>
+                        <Stack spacing={2} sx={{ width: "100%" }}>
+                          <Box
+                            sx={{
+                              marginTop: 5,
+                              display: "flex",
+                              flexDirection: "column",
+                              alignItems: "center",
+                            }}
+                          >
+                            <div style={{ marginTop: 20, marginBottom: 20 }}>
+                              <Divider variant="middle" width="600px" />
+                            </div>
+
+                            <Typography component="h1" variant="h5">
+                            {user.email}
+                            </Typography>
+                            
+                            <Typography
+                              component="h3"
+                              variant="h7"
+                              style={{ marginTop: 10, marginBottom: 10 }}
+                            >
+                              {user_type}
+                            </Typography>
+                          </Box>
+                        </Stack>
+                      </Card>
+
+                      <Card sx={{ marginTop: -70, maxWidth: 800, marginLeft: 5  }}>
+                      <Box
+                            sx={{
+                              marginTop: 0,
+                              display: "flex",
+                              flexDirection: "column",
+                              alignItems: "center",
+                            }}
+                          >
+                        <Typography component="h1" variant="h5">
+                              Summary
+                            </Typography>
+                            <div style={{ marginTop: 10, marginBottom: 20 }}>
+                              <Divider variant="middle" />
+                            </div>
+
+                              <Typography
+                                component="h3"
+                                variant="h7"
+                                style={{ marginTop: 10, marginBottom: 10 }}
+                              > {description}
+                              </Typography>
+                        </Box>
+                      </Card>
+                      <Card sx={{ marginTop: 15, maxWidth: 800, marginLeft: 5  }}>
+                      <Box
+                            sx={{
+                              marginTop: 0,
+                              display: "flex",
+                              flexDirection: "column",
+                              alignItems: "center",
+                            }}
+                          >
+
+                            <Typography component="h1" variant="h5">
+                                  Conversations
+                                </Typography>
+                                <div style={{ marginTop: 10, marginBottom: 10 }}>
+                                  <Divider variant="middle"  />
+                                </div>
+
+                                  <Typography
+                                    component="h3"
+                                    variant="h7"
+                                    style={{ marginTop: 10, marginBottom: 10 }}
+                                  > {description}
+                                  </Typography>
+                        </Box>
+                      </Card>
+                  </>
+                )} 
+                
           </Container>
         </ThemeProvider>
       </div>
